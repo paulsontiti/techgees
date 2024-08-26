3defd7287b622a51b914ed8e9866db72396f9c65
Nwoha Paulson <paulsontiti@gmail.com>
2024-08-26 09:15:04 +0100

    v1

--- actions/getRecommendedCourses.ts
+++ actions/getRecommendedCourses.ts
@@ -14,7 +14,9 @@ export type RecommendedCourseType = Course &{
 export const getRecommendedCourses = async (): Promise<ReturnValue> => {
   try {
     const recommendedCourses = await db.course.findMany({
-     
+     where:{
+      isPublished:true
+     },
           include: {
             chapters: {
               where: {
