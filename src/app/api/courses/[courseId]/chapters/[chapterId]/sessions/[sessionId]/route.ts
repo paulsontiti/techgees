bc371cf83d4ff256b013549561dcb4c9c11b15dd
Nwoha Paulson <paulsontiti@gmail.com>
2024-08-26 10:37:39 +0100

    v1

--- src/app/api/courses/[courseId]/chapters/[chapterId]/sessions/[sessionId]/route.ts
+++ src/app/api/courses/[courseId]/chapters/[chapterId]/sessions/[sessionId]/route.ts
@@ -10,7 +10,7 @@ export async function PATCH(
 ) {
   try {
     const { userId } = auth();
-    console.log(courseId)
+  
     if (!userId) return new NextResponse("Unauthorized user", { status: 401 });
 
     const { isPublished, ...values } = await req.json();
@@ -21,7 +21,7 @@ export async function PATCH(
           userId,
       },
     });
-    console.log(course)
+  
     if(!course)  return new NextResponse("Unauthorized,you are not the creator of this course",{status:401})
 
     await db.session.update({
