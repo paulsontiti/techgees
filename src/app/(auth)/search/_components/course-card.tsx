2290ddc947aa02806cd8153a32d6f9b17081d309
Nwoha Paulson <paulsontiti@gmail.com>
2024-09-10 18:44:17 +0100

    course details - complete

--- src/app/(auth)/search/_components/course-card.tsx
+++ src/app/(auth)/search/_components/course-card.tsx
@@ -2,7 +2,7 @@
 
 import CommentForm from "@/app/(course)/courses/[courseId]/_components/comment-form";
 import CourseProgress from "@/components/course-progress";
-import ErrorBoundary from "@/components/error-boundary";
+
 import IconBadge from "@/components/icon-badge";
 import PageLoader from "@/components/page-loader";
 import { formatPrice } from "@/lib/format";
@@ -47,113 +47,64 @@ function CourseCard({
     router.push(`/courses/${id}`);
   };
   return (
-    <ErrorBoundary>
-      <div
-        className="
+    <div
+      className="
             group hover:shadow-sm transition overflow-hidden border 
             rounded-lg p-3 h-full relative hover:cursor-pointer
         "
+    >
+      <PageLoader label="redirecting..." isloading={loading} />
+      <div
+        className="relative w-full aspect-video rounded-md overflow-hidden"
+        onClick={onClick}
       >
-        <PageLoader label="redirecting..." isloading={loading} />
+        <Image fill src={imageUrl} className="object-cover" alt={title} />
+      </div>
+      <div className="flex flex-col pt-2">
         <div
-          className="relative w-full aspect-video rounded-md overflow-hidden"
-          onClick={onClick}
-        >
-          <Image fill src={imageUrl} className="object-cover" alt={title} />
-        </div>
-        <div className="flex flex-col pt-2">
-          <div
-            className="text-lg md:text-base font-medium 
+          className="text-lg md:text-base font-medium 
                 group-hover:text-sky-700 transition line-clamp-2"
-          >
-            {title}
-          </div>
-          <div>
-            <h2
-              className="text-md md:text-sm italic mt-2 
+        >
+          {title}
+        </div>
+        <div>
+          <h2
+            className="text-md md:text-sm italic mt-2 
                 group-hover:text-sky-700 transition line-clamp-2 font-medium"
-            >
-              Categories:
-            </h2>
-            <div className="flex flex-wrap">
-              {Array.isArray(categories) &&
-                categories.map((category) => {
-                  return (
-                    <span
-                      key={category.id}
-                      className="text-xs mr-2 italic bg-sky-100 rounded-full mt-4 py-1 px-2"
-                    >
-                      {category.name}
-                    </span>
-                  );
-                })}
-            </div>
-          </div>
-
-          {isCombo ? (
-            <div>
-              {Array.isArray(childrenCourses) && childrenCourses.length > 0 && (
-                <div>
-                  <h2
-                    className="text-md md:text-sm italic mt-2 
-                group-hover:text-sky-700 transition line-clamp-2 font-semibold"
+          >
+            Categories:
+          </h2>
+          <div className="flex flex-wrap">
+            {Array.isArray(categories) &&
+              categories.map((category) => {
+                return (
+                  <span
+                    key={category.id}
+                    className="text-xs mr-2 italic bg-sky-100 rounded-full mt-4 py-1 px-2"
                   >
-                    Courses:
-                  </h2>
-                  <div className="flex flex-wrap">
-                    {childrenCourses.map((course) => {
-                      return (
-                        <span
-                          key={course.id}
-                          className="text-xs mr-2 italic p-2 bg-sky-100 rounded-full mt-4 py-1 px-2"
-                        >
-                          {course.title}
-                        </span>
-                      );
-                    })}
-                  </div>
-                </div>
-              )}
-            </div>
-          ) : (
-            <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
-              <div className="flex items-center gap-x-1">
-                <IconBadge size={"sm"} icon={BookOpen} />
-                {chapterslength} {chapterslength > 1 ? "chapters" : "chapter"}
-              </div>
-            </div>
-          )}
+                    {category.name}
+                  </span>
+                );
+              })}
+          </div>
+        </div>
 
-          {progressPercentage === null ? (
-            <p
-              className="text-md md:text-sm  mt-8 font-bold
-           text-slate-700"
-            >
-              {formatPrice(price)}
-            </p>
-          ) : (
-            <CourseProgress
-              value={progressPercentage ?? 0}
-              variant={progressPercentage === 100 ? "success" : "default"}
-              size="sm"
-            />
-          )}
-
-          {Array.isArray(preRequisiteCourses) &&
-            preRequisiteCourses.length > 0 && (
+        {isCombo ? (
+          <div>
+            {Array.isArray(childrenCourses) && childrenCourses.length > 0 && (
               <div>
                 <h2
                   className="text-md md:text-sm italic mt-2 
-                group-hover:text-sky-700 transition line-clamp-2"
+                group-hover:text-sky-700 transition line-clamp-2 font-semibold"
                 >
-                  Pre-requisites:
+                  Courses:
                 </h2>
                 <div className="flex flex-wrap">
-                  {preRequisiteCourses.map((course) => {
+                  {childrenCourses.map((course) => {
                     return (
                       <span
                         key={course.id}
-                        className="text-xs mr-2 italic bg-sky-100 rounded-full mt-4 py-1 px-2"
+                        className="text-xs mr-2 italic p-2 bg-sky-100 rounded-full mt-4 py-1 px-2"
                       >
                         {course.title}
                       </span>
@@ -162,17 +113,42 @@ function CourseCard({
                 </div>
               </div>
             )}
+          </div>
+        ) : (
+          <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
+            <div className="flex items-center gap-x-1">
+              <IconBadge size={"sm"} icon={BookOpen} />
+              {chapterslength} {chapterslength > 1 ? "chapters" : "chapter"}
+            </div>
+          </div>
+        )}
 
-          {Array.isArray(recommendedCourses) && recommendedCourses.length > 0 && (
+        {progressPercentage === null ? (
+          <p
+            className="text-md md:text-sm  mt-8 font-bold
+           text-slate-700"
+          >
+            {formatPrice(price)}
+          </p>
+        ) : (
+          <CourseProgress
+            value={progressPercentage ?? 0}
+            variant={progressPercentage === 100 ? "success" : "default"}
+            size="sm"
+          />
+        )}
+
+        {Array.isArray(preRequisiteCourses) &&
+          preRequisiteCourses.length > 0 && (
             <div>
               <h2
                 className="text-md md:text-sm italic mt-2 
                 group-hover:text-sky-700 transition line-clamp-2"
               >
-                Recommended courses:
+                Pre-requisites:
               </h2>
               <div className="flex flex-wrap">
-                {recommendedCourses.map((course) => {
+                {preRequisiteCourses.map((course) => {
                   return (
                     <span
                       key={course.id}
@@ -185,17 +161,39 @@ function CourseCard({
               </div>
             </div>
           )}
-          <span
-            className="mt-4"
-            onClick={() => {
-              setCommenting((val) => !val)
-            }}>
-            Add a comment
-          </span>
-          {commenting && <CommentForm courseId={id} />}
-        </div>
+
+        {Array.isArray(recommendedCourses) && recommendedCourses.length > 0 && (
+          <div>
+            <h2
+              className="text-md md:text-sm italic mt-2 
+                group-hover:text-sky-700 transition line-clamp-2"
+            >
+              Recommended courses:
+            </h2>
+            <div className="flex flex-wrap">
+              {recommendedCourses.map((course) => {
+                return (
+                  <span
+                    key={course.id}
+                    className="text-xs mr-2 italic bg-sky-100 rounded-full mt-4 py-1 px-2"
+                  >
+                    {course.title}
+                  </span>
+                );
+              })}
+            </div>
+          </div>
+        )}
+        <span
+          className="mt-4"
+          onClick={() => {
+            setCommenting((val) => !val)
+          }}>
+          Add a comment
+        </span>
+        {commenting && <CommentForm courseId={id} />}
       </div>
-    </ErrorBoundary>
+    </div>
   );
 }
 
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,21 +1,18 @@
 "use client"
 import Image from 'next/image'
 import React from 'react'
-import ErrorBoundary from './error-boundary'
 
 function Logo() {
 
-    return <ErrorBoundary>
-        <div className='flex items-center '>
-            <Image
-                height={100}
-                width={100}
-                alt="Logo"
-                src="/assets/logo.jpg"
-            />
-            {/* <span className='text-xs text-sky-700 font-bold'>theglobalgenius</span> */}
-        </div>
-    </ErrorBoundary>
+    return <div className='flex items-center '>
+        <Image
+            height={100}
+            width={100}
+            alt="Logo"
+            src="/assets/logo.jpg"
+        />
+        {/* <span className='text-xs text-sky-700 font-bold'>theglobalgenius</span> */}
+    </div>
 }
 
 export default Logo
