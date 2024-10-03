68f8e8d255a3027760f2db22cba3d82d433203f3
Nwoha Paulson <paulsontiti@gmail.com>
2024-10-03 18:40:44 +0100

    payment

--- src/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/price-form.tsx
+++ src/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/price-form.tsx
@@ -22,13 +22,13 @@ import Loader from "@/components/loader";
 import toast from "react-hot-toast";
 
 const formSchema = zod.object({
-  amount: zod.coerce.number().min(10000, {
+  amount: zod.coerce.number().min(1, {
     message: "amount is required",
   }),
 });
 
-function PriceForm({email,courseId,chapterId}:{
-    email:string,courseId:string,chapterId:string
+function PriceForm({ email, courseId, chapterId }: {
+  email: string, courseId: string, chapterId: string
 }) {
 
   const form = useForm<zod.infer<typeof formSchema>>({
@@ -46,15 +46,15 @@ function PriceForm({email,courseId,chapterId}:{
       const response = await axios.post(
         '/api/paystack/create-checkout-session',
         {
-         
+
           amount: values.amount,
           email,
           courseId
         }
       );
 
-      const { authorizationUrl,reference } = response.data;
-      
+      const { authorizationUrl, reference } = response.data;
+
 
       // Open Paystack payment page in a new tab
       const paymentWindow = window.open(authorizationUrl);
@@ -69,53 +69,53 @@ function PriceForm({email,courseId,chapterId}:{
       } else {
         toast.error('Failed to open payment window.Try again');
       }
-    } catch (error:any) {
-      toast.error('Error initializing payment: '+ error.message);
+    } catch (error: any) {
+      toast.error('Error initializing payment: ' + error.message);
       // Handle the error, e.g., show a user-friendly error message to the user.
     } finally {
     }
   };
 
- 
+
   return (
-     <div
+    <div
       className="border bg-slate-100 rounded-md p-4 w-[350px]"
     >
-   
-        <Form {...form}>
-          <form
-             onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-4 mt-4"
-          >
-            <FormField
-              control={form.control}
-              name="amount"
-              render={({ field }) => {
-                return (
-                  <FormItem>
-                    <FormLabel>Amount</FormLabel>
-                    <FormControl>
-                      <Input
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4 mt-4"
+        >
+          <FormField
+            control={form.control}
+            name="amount"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input
                       type="number"
                       step={10000}
-                        disabled={isSubmitting}
-                        placeholder='e.g. "10000"'
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormDescription>Note: You will be paying to Black Wizards Technology</FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
-            <div className="flex items-center gap-x-2">
+                      disabled={isSubmitting}
+                      placeholder='e.g. "10000"'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>Note: You will be paying to Black Wizards Technology</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <div className="flex items-center gap-x-2">
             <Button type="submit" disabled={!isValid || isSubmitting}>
-                Pay with Paystack <Loader loading={isSubmitting} />
-              </Button>
-            </div>
-          </form>
-        </Form>
+              Pay with Paystack <Loader loading={isSubmitting} />
+            </Button>
+          </div>
+        </form>
+      </Form>
 
     </div>
   );
