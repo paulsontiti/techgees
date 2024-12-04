08fd4d1eaf5929d18d5a2349addc5fbb72fe7acc
Nwoha Paulson <paulsontiti@gmail.com>
2024-12-04 17:41:44 +0100

    menu button error solution

--- src/app/(root)/course/[courseId]/_components/sidebar.tsx
+++ src/app/(root)/course/[courseId]/_components/sidebar.tsx
@@ -1,18 +1,24 @@
-
-import DashboardLink from "@/app/(root)/_components/dashboard-link";
+"use client"
 import SidebarRoutes from "./sibebar-routes"
 import SignIn from "@/app/(root)/_components/sign-in-button";
 import SignUp from "@/app/(root)/_components/sign-up-button";
 import { getUserCookie } from "@/lib/get-user-cookie";
+import { Button } from "@/components/ui/button";
+import { useCurrentUser } from "../../../../../../store/current-user-store";
 
 export const Sidebar = async()=>{
-
+    const {logout} = useCurrentUser();
     const userId = await getUserCookie();
     return <div className="
         h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm
     ">
         <div className="p-6">
-        {!userId &&
+        {userId ?  <div className="flex items-center gap-x-4">
+              <Button 
+                onClick={logout}
+              >Logout</Button>
+
+            </div>:
             <div className="flex items-center gap-x-4">
               <SignIn />
               <SignUp />
