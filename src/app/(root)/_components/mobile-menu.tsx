eb3712d91ecaadb06ff9113d03704f70665aaebb
Nwoha Paulson <paulsontiti@gmail.com>
2024-12-16 14:28:17 +0100

    sheet error

--- src/app/(root)/_components/mobile-menu.tsx
+++ src/app/(root)/_components/mobile-menu.tsx
@@ -1,13 +1,12 @@
-"use client"
+
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { Menu } from 'lucide-react'
 import React from 'react'
 import { Sidebar } from '../course/[courseId]/_components/sidebar'
-import { useSheetStore } from '../../../../store/sheet-store'
 
 
 async function MobileMenu() {
-//const {isOpen,openSheet} = useSheetStore();
+
   return (
     <Sheet >
         <SheetTrigger className='lg:hidden pr-4 hover:opacity-75 transition'>
