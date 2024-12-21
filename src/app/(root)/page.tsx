6f6f663310977d0110ba462562a0b9183aa86c3a
Nwoha Paulson <paulsontiti@gmail.com>
2024-12-21 14:15:03 +0100

    device address

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -8,13 +8,22 @@ import HomepageCourses from "./_components/homepage-courses";
 import FreeCourses from "./_components/free-courses";
 import { getAchievements } from "../../../actions/getAchievements";
 import Achievement from "./_components/stat";
+import { ip, ipv6, mac } from 'address';
 
 export default async function Home() {
 
     const {achevements,error} = await getAchievements();
 
-  
+    const ipAddress = ip();   // '192.168.0.2'
+    const ipV6 = ipv6(); // 'fe80::7aca:39ff:feb0:e67d'
+   let macAddr =  "";
+   mac(function (err, addr) {
+      macAddr = addr || ""; // '78:ca:39:b0:e6:7d'
+    });
     return <div className="bg-[#EFF6FF]">
+        <p>{ipAddress}</p>
+        <p>{ipV6}</p>
+        <p>{macAddr}</p>
 
     <WelcomeMessage />
    
