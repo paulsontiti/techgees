5372a25eaf871a5ad5e33d774455a35b3c61e530
Nwoha Paulson <paulsontiti@gmail.com>
2025-01-30 17:58:08 +0100

    wallet,network

--- src/components/earning-ad.tsx
+++ src/components/earning-ad.tsx
@@ -10,7 +10,7 @@ function  EarningAd() {
                     <CardTitle className={`${textSecondaryColor} text-2xl`}>Earn more rewards</CardTitle>
                     <CardDescription className="text-white text-xl">By referring a friend to The Global Genius</CardDescription>
                 </CardHeader>
-                <CardContent className="text-white">
+                <CardContent className="text-white lg:w-1/2">
                     With our referral system, you can earn upto 10% when you grow a network of friends who buy our courses.
                     Ensure they select your username as their referer
                 </CardContent>
