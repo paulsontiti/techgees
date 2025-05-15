e1e9719c7cba964e5aeb5047741a0dc1bfa629ae
Nwoha Paulson <paulsontiti@gmail.com>
2025-05-15 12:08:50 +0100

    Course chapters

--- actions/getCourseWithProgressChapters.ts
+++ actions/getCourseWithProgressChapters.ts
@@ -1,123 +1,124 @@
 import { db } from "@/lib/db";
-import {Course } from "@prisma/client";
+import { Course } from "@prisma/client";
 import { getCourseProgress } from "./getCourseProgress";
 
-
-interface ReturnValue{
-    courses: SearchPageCourseType[],
-    error:Error | null
+interface ReturnValue {
+  courses: SearchPageCourseType[];
+  error: Error | null;
 }
 
-export type SearchPageCourseType = Course &{
-        chapters:{id:string}[],
-        progressPercentage:number | null,
-    } | null
+export type SearchPageCourseType =
+  | (Course & {
+      chapters: { id: string }[];
+      progressPercentage: number | null;
+    })
+  | null;
 
-type ParamType ={
-    userId:string,
-    title?:string,
-    categoryId?:string
-}
+type ParamType = {
+  userId: string;
+  title?: string;
+  categoryId?: string;
+};
 
-export const getCourseWithProgressChapters = async(
-    {userId,title,categoryId}:ParamType
-):
-Promise<ReturnValue>=>{
-    try{
+export const getCourseWithProgressChapters = async ({
+  userId,
+  title,
+  categoryId,
+}: ParamType): Promise<ReturnValue> => {
+  try {
+    const courseCategories = await db.courseCategory.findMany({
+      where: {
+        categoryId,
+      },
+      include: {
+        course: true,
+      },
+    });
 
-        
-const courseCategories = await db.courseCategory.findMany({
-    where:{
-        categoryId
-    },include:{
-        course:true,
-        }
-    
-})
+    const courseIds = courseCategories.map((cc) => cc.course.id);
 
-
-const courseIds = courseCategories.map((cc) => cc.course.id)
-
-
-let courses:any[] = []
-if(title){
-courses = await db.course.findMany({
-    where:{
-       
-        title:{
-            contains:title,
-             mode:"insensitive"
+    let courses: any[] = [];
+    if (title) {
+      courses = await db.course.findMany({
+        where: {
+          title: {
+            contains: title,
+            mode: "insensitive",
+          },
+          isPublished: true,
         },
-        isPublished:true,
-    },include:{
-        chapters:{
-            where:{
-                isPublished:true,
-
-            },select:{
-                id:true
-            }
-        },paystackPayments:{
-            where:{
-                userId
-            }
-        }
-    },orderBy:{
-        createdAt:"desc"
-    }
-})
-}else{
-    courses = await db.course.findMany({
-        where:{
-            id:{
-                in: courseIds
+        include: {
+          chapters: {
+            where: {
+              //isPublished: true,
             },
-            isPublished:true,
-        },include:{
-            chapters:{
-                where:{
-                    isPublished:true,
-    
-                },select:{
-                    id:true
-                }
-            },paystackPayments:{
-                where:{
-                    userId
-                }
-            }
-        },orderBy:{
-            createdAt:"desc"
-        }
-    })
-}
+            select: {
+              id: true,
+            },
+          },
+          paystackPayments: {
+            where: {
+              userId,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    } else {
+      courses = await db.course.findMany({
+        where: {
+          id: {
+            in: courseIds,
+          },
+          isPublished: true,
+        },
+        include: {
+          chapters: {
+            where: {
+              isPublished: true,
+            },
+            select: {
+              id: true,
+            },
+          },
+          paystackPayments: {
+            where: {
+              userId,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    }
 
-
-const coursesWithProgressAndCategory:SearchPageCourseType[]  = 
-await Promise.all(
-    courses.map(async(course)=>{
-        if(course.paystackPayments.length === 0){
+    const coursesWithProgressAndCategory: SearchPageCourseType[] =
+      await Promise.all(
+        courses.map(async (course) => {
+          if (course.paystackPayments.length === 0) {
             return {
-                    ...course,
-                    progressPercentage:null
-                }
-            
-        }
+              ...course,
+              progressPercentage: null,
+            };
+          }
 
-        const {progressPercentage} = await getCourseProgress(userId,course.id)
-        return {
-                ...course,
-            progressPercentage
-            }
-        
-    })
-)
+          const { progressPercentage } = await getCourseProgress(
+            userId,
+            course.id
+          );
+          return {
+            ...course,
+            progressPercentage,
+          };
+        })
+      );
 
-
-       
-      return {courses:coursesWithProgressAndCategory,error:null}
-    }catch(error:any){
-    console.log("[getCourseWithProgressChaptersCategory]",error)
-        return {courses:[],error}
-    }
-    }
+    return { courses: coursesWithProgressAndCategory, error: null };
+  } catch (error: any) {
+    console.log("[getCourseWithProgressChaptersCategory]", error);
+    return { courses: [], error };
+  }
+};
--- src/app/(root)/course/[courseId]/_components/course-details.tsx
+++ src/app/(root)/course/[courseId]/_components/course-details.tsx
@@ -1,106 +1,135 @@
-"use client"
-import { Button } from '@/components/ui/button';
-import { bgNeutralColor, bgNeutralColor2, textPrimaryColor } from '@/utils/colors';
-import React, { useEffect, useState } from 'react'
-import EnrollButton from './enroll-button';
-import VideoPlayer from '@/components/video-player';
-import { Preview } from '@/components/preview';
-import { Card, CardContent, CardHeader } from '@/components/ui/card';
-import { Check } from 'lucide-react';
-import { CourseContentAccordion } from './course-content-accordion';
-import { ChapterContentAccordion } from './chapter-content-accordion';
-import { CourseType } from '../../../../../../actions/getCourse';
-import { Course } from '@prisma/client';
-import axios from 'axios';
-import { Skeleton } from '@/components/ui/skeleton';
-import Separator from '@/components/separator';
-import CourseChaptersAndSessionsDetails from '@/components/course/course-chapters-sessions-details';
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  bgNeutralColor,
+  bgNeutralColor2,
+  textPrimaryColor,
+} from "@/utils/colors";
+import React, { useEffect, useState } from "react";
+import EnrollButton from "./enroll-button";
+import VideoPlayer from "@/components/video-player";
+import { Preview } from "@/components/preview";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Check } from "lucide-react";
+import { CourseContentAccordion } from "./course-content-accordion";
+import { ChapterContentAccordion } from "./chapter-content-accordion";
+import { CourseType } from "../../../../../../actions/getCourse";
+import { Course } from "@prisma/client";
+import axios from "axios";
+import { Skeleton } from "@/components/ui/skeleton";
+import Separator from "@/components/separator";
+import CourseChaptersAndSessionsDetails from "@/components/course/course-chapters-sessions-details";
 
- function CourseDetails({courseId}:{courseId:string}) {
-const [course,setCourse] = useState<CourseType | null | undefined>(undefined);
-const [startedCourse,setStartedCourse] = useState<boolean | undefined>(undefined);
-const [courseChildren,setCourseChildren] = useState<Course[] | undefined>(undefined);
+function CourseDetails({ courseId }: { courseId: string }) {
+  const [course, setCourse] = useState<CourseType | null | undefined>(
+    undefined
+  );
+  const [startedCourse, setStartedCourse] = useState<boolean | undefined>(
+    undefined
+  );
+  const [courseChildren, setCourseChildren] = useState<Course[] | undefined>(
+    undefined
+  );
 
-
-useEffect(()=>{
-    (
-        async()=>{
-           try{
-            const res = await axios.get(`/api/courses/${courseId}/started-course`);
-            setStartedCourse(res.data);
-           }catch(err){
-            setStartedCourse(false);
-           }
-        }
-    )()
-},[]);
-
-//get course
-useEffect(()=>{
-    (
-        async()=>{
-           try{
-            const res = await axios.get(`/api/courses/${courseId}`);
-            setCourse(res.data);
-           }catch(err){
-            setCourse(null);
-           }
-        }
-    )()
-},[]);
-  
-//get course children
-useEffect(()=>{
-    (
-        async()=>{
-           try{
-            const res = await axios.get(`/api/courses/${courseId}/course-children`);
-            setCourseChildren(res.data);
-           }catch(err){
-            setCourseChildren([]);
-           }
-        }
-    )()
-},[]);
-
-   
-  return (
-    <div className={`flex flex-col  items-center justify-center ${bgNeutralColor} p-4`}>
-    <div className="border-black border-2 my-16 w-full md:w-[400px] py-8 px-2 rounded-xl 
-      flex items-center justify-around gap-x-2">
-
-      <div className="flex flex-col items-center justify-center gap-y-4 ">
-        <h2 className={`${textPrimaryColor}`}>Course status</h2>
-        {startedCourse !== undefined ? <Button variant={`${startedCourse ? "success" : "destructive"}`} size="sm">
-        {`${startedCourse ? "Started" : "Not started"}`}</Button> : <Skeleton className='w-32 h-10 bg-red-300'/>}
-      </div>
-      <Separator />
-
-      <div className="flex flex-col items-center justify-center gap-y-4">
-        <h2 className={`${textPrimaryColor}`}>Get started</h2>
-        <EnrollButton courseId={courseId} label={`${startedCourse ? "Go to class" : "Start for free"}`} />
-
-      </div>
-    </div>
-    <div className="w-full md:w-[700px] xl:w-[900px]">
-     {course !== undefined ?  <div className="my-4 w-full">
-        <VideoPlayer url={course?.overviewVideoUrl ?? ""} title="Course overview"/>
-      </div> : 
-      <Skeleton className={`w-full my-4 h-72 ${bgNeutralColor2}`}/>
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get(`/api/courses/${courseId}/started-course`);
+        setStartedCourse(res.data);
+      } catch (err) {
+        setStartedCourse(false);
       }
+    })();
+  }, []);
 
-     {course !== undefined ?  <EnrollButton courseId={course?.id || ""} label={`${startedCourse ? "Go to class" : "Start for free"}`}/> : 
-     <Skeleton className={`w-full h-10 ${bgNeutralColor2}`}/>
-     }
-      <div className="my-8 p-4">
-        <h1 className={`${textPrimaryColor} text-xl font-bold`}>Course Details</h1>
-       {
-        course !== undefined ?  <Preview value={course?.description ?? ""} /> : 
-        <Skeleton className={`w-full mt-4 h-[200px] ${bgNeutralColor2}`}/>
-       }
+  //get course
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get(`/api/courses/${courseId}`);
+        setCourse(res.data);
+      } catch (err) {
+        setCourse(null);
+      }
+    })();
+  }, []);
+
+  //get course children
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get(`/api/courses/${courseId}/course-children`);
+        setCourseChildren(res.data);
+      } catch (err) {
+        setCourseChildren([]);
+      }
+    })();
+  }, []);
+
+  return (
+    <div
+      className={`flex flex-col  items-center justify-center ${bgNeutralColor} p-4`}
+    >
+      <div
+        className="border-black border-2 my-16 w-full md:w-[400px] py-8 px-2 rounded-xl 
+      flex items-center justify-around gap-x-2"
+      >
+        <div className="flex flex-col items-center justify-center gap-y-4 ">
+          <h2 className={`${textPrimaryColor}`}>Course status</h2>
+          {startedCourse !== undefined ? (
+            <Button
+              variant={`${startedCourse ? "success" : "destructive"}`}
+              size="sm"
+            >
+              {`${startedCourse ? "Started" : "Not started"}`}
+            </Button>
+          ) : (
+            <Skeleton className="w-32 h-10 bg-red-300" />
+          )}
+        </div>
+        <Separator />
+
+        <div className="flex flex-col items-center justify-center gap-y-4">
+          <h2 className={`${textPrimaryColor}`}>Get started</h2>
+          <EnrollButton
+            courseId={courseId}
+            label={`${startedCourse ? "Go to class" : "Start for free"}`}
+          />
+        </div>
       </div>
-    {
-        course !== undefined ?   Array.isArray(course?.courseBenefits) && course.courseBenefits.length > 0 &&
+      <div className="w-full md:w-[700px] xl:w-[900px]">
+        {course !== undefined ? (
+          <div className="my-4 w-full">
+            <VideoPlayer
+              url={course?.overviewVideoUrl ?? ""}
+              title="Course overview"
+            />
+          </div>
+        ) : (
+          <Skeleton className={`w-full my-4 h-72 ${bgNeutralColor2}`} />
+        )}
+
+        {course !== undefined ? (
+          <EnrollButton
+            courseId={course?.id || ""}
+            label={`${startedCourse ? "Go to class" : "Start for free"}`}
+          />
+        ) : (
+          <Skeleton className={`w-full h-10 ${bgNeutralColor2}`} />
+        )}
+        <div className="my-8 p-4">
+          <h1 className={`${textPrimaryColor} text-xl font-bold`}>
+            Course Details
+          </h1>
+          {course !== undefined ? (
+            <Preview value={course?.description ?? ""} />
+          ) : (
+            <Skeleton className={`w-full mt-4 h-[200px] ${bgNeutralColor2}`} />
+          )}
+        </div>
+        {course !== undefined ? (
+          Array.isArray(course?.courseBenefits) &&
+          course.courseBenefits.length > 0 && (
             <Card className="mt-4 w-full">
               <CardHeader className="text-xl font-bold">
                 Benefits of taking this course
@@ -108,7 +137,10 @@ useEffect(()=>{
               <CardContent className="flex flex-col">
                 {course?.courseBenefits.map((benefit) => {
                   return (
-                    <div key={benefit.id} className="flex items-start my-2 gap-2">
+                    <div
+                      key={benefit.id}
+                      className="flex items-start my-2 gap-2"
+                    >
                       <Check className="max-w-4 max-h-4 min-w-4 min-h-4" />
                       <div className="text-xs md:text-sm">{benefit.text}</div>
                     </div>
@@ -116,30 +148,44 @@ useEffect(()=>{
                 })}
               </CardContent>
             </Card>
-    
-:
-<Skeleton className={`w-full mt-4 h-[200px] ${bgNeutralColor2}`}/>
-    }
-      <div className="mt-8 p-4">
-        <h1 className={`text-xl font-bold ${textPrimaryColor}`}>Course content</h1>
+          )
+        ) : (
+          <Skeleton className={`w-full mt-4 h-[200px] ${bgNeutralColor2}`} />
+        )}
+        <div className="mt-8 p-4">
+          <h1 className={`text-xl font-bold ${textPrimaryColor}`}>
+            Course content
+          </h1>
+        </div>
+        {courseChildren?.length === 0 && (
+          <CourseChaptersAndSessionsDetails courseId={courseId} />
+        )}
+        {courseChildren !== undefined ? (
+          <>
+            {courseChildren?.length > 0
+              ? courseChildren.map((course, index) => {
+                  return (
+                    <CourseContentAccordion
+                      courseId={course.id}
+                      courseTitle={course.title}
+                      key={index}
+                    />
+                  );
+                })
+              : course?.chapters.map((chapter) => {
+                  return (
+                    <ChapterContentAccordion
+                      chapter={chapter}
+                      key={chapter.id}
+                    />
+                  );
+                })}
+          </>
+        ) : (
+          <Skeleton className={`w-full mt-4 h-[200px] ${bgNeutralColor2}`} />
+        )}
 
-      </div>
-   {courseChildren?.length === 0 &&    <CourseChaptersAndSessionsDetails courseId={courseId}/>}
-      {courseChildren !== undefined ? <>
-        {
-        courseChildren?.length > 0 ?
-        courseChildren.map((course, index) => {
-
-            return <CourseContentAccordion courseId={course.id} courseTitle={course.title} key={index} />
-          })
-          :
-          course?.chapters.map((chapter) => {
-            return <ChapterContentAccordion chapter={chapter} key={chapter.id} />;
-          })}
-      </> : <Skeleton className={`w-full mt-4 h-[200px] ${bgNeutralColor2}`}/>
-}
-
-      {/* <div>
+        {/* <div>
         <h1 className={`text-lg font-semibold mt-8 mb-2 ${textPrimaryColor}`}>Pre-requisite</h1>
         {preRequisiteCourses.length > 0 ? preRequisiteCourses.map((course) => {
           return <Link href={`/course/${course.id}`}
@@ -150,7 +196,7 @@ useEffect(()=>{
           <p className="text-xs md:text-sm">None</p>}
       </div> */}
 
-      {/* <div className="bg-white p-4 mt-8">
+        {/* <div className="bg-white p-4 mt-8">
         <h1 className={`text-lg font-semibold mb-2 ${textPrimaryColor}`}>Recommended courses</h1>
         {recommendedCourses.length > 0 ?
           <div className="flex items-center flex-wrap gap-4">
@@ -169,12 +215,11 @@ useEffect(()=>{
           <p className="text-xs md:text-sm">None</p>}
       </div> */}
 
-
-     {/* <div className="mt-8">
+        {/* <div className="mt-8">
      <EnrollButton courseId={course?.id} label={`${startedCourse ? "Go to class" : "Start for free"}`}/>
      </div> */}
 
-      {/* {Array.isArray(comments) && comments.length > 0 && <div className="mt-4 border p-2 max-w-full">
+        {/* {Array.isArray(comments) && comments.length > 0 && <div className="mt-4 border p-2 max-w-full">
         <h1 className={`text-lg font-semibold mt-8 mb-2 ${textPrimaryColor}`}>Reviews</h1>
         {comments.map((comment, index) => {
 
@@ -182,9 +227,9 @@ useEffect(()=>{
         })}
       </div>
       } */}
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default CourseDetails
+export default CourseDetails;
