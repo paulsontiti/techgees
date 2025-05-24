f858297399962de3f4e3bafe914eae1c3394a830
Nwoha Paulson <paulsontiti@gmail.com>
2025-05-24 14:52:38 +0100

    View Course - Link Button

--- src/app/(root)/scholarships/[scholarshipId]/page.tsx
+++ src/app/(root)/scholarships/[scholarshipId]/page.tsx
@@ -1,83 +1,83 @@
 import React from "react";
 import { getScholarshipById } from "../../../../../actions/getScholarshipById";
 import ErrorPage from "@/components/error";
-import { redirect} from "next/navigation";
+import { redirect } from "next/navigation";
 import Image from "next/image";
 import { formatPrice } from "@/lib/format";
 import { Preview } from "@/components/preview";
 import { getUserCookie } from "@/lib/get-user-cookie";
 import VerifyPayment from "@/app/(course)/courses/components/verify-payment";
 import ApplyButton from "../_components/apply-button";
-import { Button } from "@/components/ui/button";
 import { getCourseByScholarshipById } from "../../../../../actions/getCourseByScholarshipId";
-import Link from "next/link";
+
+import LinkButton from "@/components/link-button";
 
 async function ScholarshipPage({
   params: { scholarshipId },
-  searchParams:{reference}
+  searchParams: { reference },
 }: {
   params: { scholarshipId: string };
-  searchParams: { reference: string,redirectUrl:string }
+  searchParams: { reference: string; redirectUrl: string };
 }) {
-
   const userId = await getUserCookie();
-  
+
   const { scholarship, error } = await getScholarshipById(scholarshipId);
 
   if (error) return <ErrorPage name={error.name} />;
 
-    const { course, error:couError } = await getCourseByScholarshipById(scholarshipId);
+  const { course, error: couError } = await getCourseByScholarshipById(
+    scholarshipId
+  );
 
   if (couError) return <ErrorPage name={couError.name} />;
 
-
   if (!scholarship || !course) return redirect("/scholarships");
-
 
   const url = process.env.WEB_URL!;
 
   return (
     <section className="p-4 flex flex-col items-center justify-center">
-    <div className="md:w-10/12 lg:w-8/12 xl:w-6/12">
-        <VerifyPayment reference={reference} redirectUrl={`/scholarships/${scholarshipId}`}/>
-      
-      <h2 className="font-bold text-2xl mb-4 text-center">{scholarship.title}</h2>
-      <div className="w-full grid md:grid-cols-2">
-        <div>
-          <div className="relative w-full aspect-video rounded-md overflow-hidden">
-            <Image
-              fill
-              src={scholarship.imageUrl ?? ""}
-              className="object-cover"
-              alt={scholarship.title}
+      <div className="md:w-10/12 lg:w-8/12 xl:w-6/12">
+        <VerifyPayment
+          reference={reference}
+          redirectUrl={`/scholarships/${scholarshipId}`}
+        />
+
+        <h2 className="font-bold text-2xl mb-4 text-center">
+          {scholarship.title}
+        </h2>
+        <div className="w-full grid md:grid-cols-2">
+          <div>
+            <div className="relative w-full aspect-video rounded-md overflow-hidden">
+              <Image
+                fill
+                src={scholarship.imageUrl ?? ""}
+                className="object-cover"
+                alt={scholarship.title}
+              />
+            </div>
+            <h3 className="my-2">{scholarship.subTitle}</h3>
+            <p>
+              Registration fee :{" "}
+              <strong>{formatPrice(scholarship.price || 0)}</strong>
+            </p>
+
+            <ApplyButton
+              url={url}
+              terms={scholarship.terms || ""}
+              scholarshipId={scholarshipId}
+              userId={userId || ""}
+              price={scholarship.price!}
             />
           </div>
-          <h3 className="my-2">{scholarship.subTitle}</h3>
-          <p>
-            Registration fee :{" "}
-            <strong>{formatPrice(scholarship.price || 0)}</strong>
-          </p>
-         
-           <ApplyButton 
-           url={url}
-          terms={scholarship.terms || ""}
-          scholarshipId={scholarshipId} userId={userId || ""} price={scholarship.price!}/>
-         
+          <div>
+            <Preview value={scholarship.description || ""} />
+          </div>
+          <LinkButton label="Visit Course" url={`/courses/${course.id}`} />
         </div>
-        <div>
-          <Preview value={scholarship.description || ""} />
-        </div>
-
-        <Button 
-        >
-          <Link href={`/courses/${course.id}`}>Visit Course</Link>
-        </Button>
       </div>
-    </div>
     </section>
   );
 }
 
 export default ScholarshipPage;
-
-
--- src/components/link-button.tsx
+++ src/components/link-button.tsx
@@ -1,30 +1,28 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-import { Button } from './ui/button'
-import Loader from './loader'
-import { useRouter } from 'next/navigation'
-import ErrorBoundary from './error-boundary'
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import Loader from "./loader";
+import { useRouter } from "next/navigation";
 
-function LinkButton({ label, url }: { label: string, url: string }) {
-  const [isRedirecting, setRedirecting] = useState(false)
-  const router = useRouter()
+function LinkButton({ label, url }: { label: string; url: string }) {
+  const [isRedirecting, setRedirecting] = useState(false);
+  const router = useRouter();
 
   const onClick = () => {
-    setRedirecting(true)
-    router.push(url)
-  }
+    setRedirecting(true);
+    router.push(url);
+  };
 
   return (
-    <ErrorBoundary>
-      <div className='flex items-center justify-between'
-        onClick={onClick}
-      >
-        <Button variant="ghost" size="sm">{label}</Button>
-        <Loader loading={isRedirecting} />
-      </div>
-    </ErrorBoundary>
-  )
+    <Button
+      className="flex items-center justify-center gap-x-2 my-4"
+      onClick={onClick}
+      size="sm"
+    >
+      {label} <Loader loading={isRedirecting} />
+    </Button>
+  );
 }
 
-export default LinkButton
+export default LinkButton;
