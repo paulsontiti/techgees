b5bab5725f51b5f54673f2a8642aa94aabedcaf5
Nwoha Paulson <paulsontiti@gmail.com>
2025-05-31 10:16:39 +0100

    Earning - Payee-name

--- src/app/api/user/earnings/payee-name/[paystackPaymentId]/route.ts
+++ src/app/api/user/earnings/payee-name/[paystackPaymentId]/route.ts
@@ -1,36 +1,43 @@
-import { db } from "@/lib/db"
-import { NextResponse } from "next/server"
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
 
-export async function GET(req: Request, {
-    params: { paystackPaymentId }
-}: {
-    params: { paystackPaymentId: string }
-}) {
+export async function GET(
+  req: Request,
+  {
+    params: { paystackPaymentId },
+  }: {
+    params: { paystackPaymentId: string };
+  }
+) {
+  try {
+    const payment = await db.paystackPayment.findUnique({
+      where: {
+        id: paystackPaymentId,
+      },
+      select: {
+        userId: true,
+      },
+    });
 
-    try {
-        const payment = await db.paystackPayment.findUnique({
-
-            where: {
-                id: paystackPaymentId
-            }, select: {
-                userId: true
-            }
-        })
-
-        const user = await db.dBUser.findUnique({
-            where:{
-                userId: payment?.userId
-            }
-        })
-        const firstName = user?.firstName ?? "";
-        const lastName = user?.lastName ?? ""
-        const email = user?.email
-        return NextResponse.json(firstName ? `${firstName} ${lastName ?? ""}` : email);
-    } catch (err) {
-
-        console.log("[GET_PAYEE_NAME]", err)
-        return new NextResponse("Internal Error", {
-            status: 500
-        })
+    if (!payment) {
+      return NextResponse.json("");
     }
+
+    const user = await db.dBUser.findUnique({
+      where: {
+        userId: payment?.userId,
+      },
+    });
+    const firstName = user?.firstName ?? "";
+    const lastName = user?.lastName ?? "";
+    const email = user?.email;
+    return NextResponse.json(
+      firstName ? `${firstName} ${lastName ?? ""}` : email
+    );
+  } catch (err) {
+    console.log("[GET_PAYEE_NAME]", err);
+    return new NextResponse("Internal Error", {
+      status: 500,
+    });
+  }
 }
--- src/app/api/user/earnings/payment-amount/[paystackPaymentId]/route.ts
+++ src/app/api/user/earnings/payment-amount/[paystackPaymentId]/route.ts
@@ -1,29 +1,32 @@
-import { db } from "@/lib/db"
-import { NextResponse } from "next/server"
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
 
-export async function GET(req: Request, {
-    params: { paystackPaymentId }
-}: {
-    params: { paystackPaymentId: string }
-}) {
-
-    try {
-        const payment = await db.paystackPayment.findUnique({
-
-            where: {
-                id: paystackPaymentId
-            }, select: {
-                amount:true
-            }
-        })
-
-       
-        return NextResponse.json(payment?.amount);
-    } catch (err) {
-
-        console.log("[GET_PAYMENT_AMOUNT]", err)
-        return new NextResponse("Internal Error", {
-            status: 500
-        })
+export async function GET(
+  req: Request,
+  {
+    params: { paystackPaymentId },
+  }: {
+    params: { paystackPaymentId: string };
+  }
+) {
+  try {
+    const payment = await db.paystackPayment.findUnique({
+      where: {
+        id: paystackPaymentId,
+      },
+      select: {
+        amount: true,
+      },
+    });
+    if (!payment) {
+      return NextResponse.json(0);
     }
+
+    return NextResponse.json(payment?.amount);
+  } catch (err) {
+    console.log("[GET_PAYMENT_AMOUNT]", err);
+    return new NextResponse("Internal Error", {
+      status: 500,
+    });
+  }
 }
