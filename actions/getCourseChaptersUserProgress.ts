f7171a994dae3fafaee94d1c0a1adc2c62ea7dc7
Nwoha Paulson <paulsontiti@gmail.com>
2026-06-12 18:12:32 +0100

    fetch unplished chapters

--- actions/getCourseChaptersUserProgress.ts
+++ actions/getCourseChaptersUserProgress.ts
@@ -24,7 +24,7 @@ export const getCourseChaptersUserProgress = async (
       },
       include: {
         chapters: {
-          where: { isPublished: true },
+          //where: { isPublished: true },
           include: {
             userProgresses: {
               where: {
@@ -33,7 +33,7 @@ export const getCourseChaptersUserProgress = async (
             },
             sessions: {
               where: {
-                isPublished:true
+                //isPublished:true
               },
               include:{
                 userProgresses: {
