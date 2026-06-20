3d9e4cba88853152c8d5773204e40729240bc8b4
Nwoha Paulson <paulsontiti@gmail.com>
2026-06-20 15:24:41 +0100

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "The Global Genius",
   description: "A Learning Management System, a platform where you can learn any and everything with its pioneer in TECH and Software Development",
+  //  icons: {
+  //   icon: "/favicon.ico",
+  //   shortcut: "/favicon.ico",
+  //   apple: "/apple-touch-icon.png",
+  // },
 };
 
 
